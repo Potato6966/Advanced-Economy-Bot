1a86de929ae367b1b62c00089bcee244547b83fe
MaxDev6966 <85440084+MaxGamingOfficalYT@users.noreply.github.com>
2022-11-25 22:43:15 +0000

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.execute = async (client, message, args) => {
+    const embed = new MessageEmbed()
+        .setAuthor("Commands")
+        .setDescription(`Total Commands: ${client.commands.size}`)
+        .setColor("BLURPLE")
+        .setTimestamp()
+        .setThumbnail(client.user.displayAvatarURL)
+        .setFooter(message.author.tag, message.author.displayAvatarURL);
+    client.commands.forEach(cmd => {
+        embed.addField(`${cmd.help.name}`, `Aliases: ${cmd.help.aliases.join(", ") || "None"}\nUsage: \`${client.prefix}${cmd.help.usage}\``, true);
+    });
+    return message.channel.send(embed);
+}
+
+exports.help = {
+    name: "help",
+    aliases: ["h"],
+    usage: `help`
+}
