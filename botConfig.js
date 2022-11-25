d7d468f7c08bd85c11df14b8952a70ee9b6b9c7d
MaxDev6966 <85440084+MaxGamingOfficalYT@users.noreply.github.com>
2022-11-25 22:41:43 +0000

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "OTY5NjA5MTQ2MDUzODQwOTA2.G48N0y.Vg5N6KBqU7_hlY4rJnkhJrm7dikJFyz4h4eZ1U",
+    prefix: "~",
+    admins: [
+        "806810037459746846"
+],
+    debug: false,
+    countChannel: ""
+};
